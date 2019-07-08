b18dcfd3fbd5bf2c0241c9d35e2bf6c7bffd603e
Daniel Clarke <deliotclarke@gmail.com>
2019-07-08 14:05:48 -0500

    added gitignore and finished exercises

--- Program.cs
+++ Program.cs
@@ -40,6 +40,52 @@ namespace lists
       //interestingly, planet list still contains all planets despite using get range to remove all the rocky planets
       Console.WriteLine("End of planets exercise");
 
+      //start of Random Numbers exercise
+
+      Random random = new Random();
+      List<int> numbers = new List<int> {
+        random.Next(6),
+        random.Next(6),
+        random.Next(6),
+        random.Next(6),
+        random.Next(6),
+        random.Next(6),
+      };
+
+      Console.WriteLine("Using Contains with if/else: ");
+      for (int i = 0; i < numbers.Count; i++)
+      {
+        if (numbers.Contains(i))
+        {
+          Console.WriteLine($"List does contain {i}");
+        }
+        else
+        {
+          Console.WriteLine($"List does not contain {i}");
+        }
+      }
+
+      Console.WriteLine("Using Contains with Ternary: ");
+      for (int i = 0; i < numbers.Count; i++)
+      {
+        string output = numbers.Contains(i) ? $"List does contain {i}" : $"List does not contain {i}";
+        Console.WriteLine(output);
+      }
+
+      // try to use .exists
+      Console.WriteLine("Using Exists: ");
+      for (int i = 0; i < numbers.Count; i++)
+      {
+        if (numbers.Exists(x => x == i))
+        {
+          Console.WriteLine($"{i} exists in our List");
+        }
+        else
+        {
+          Console.WriteLine($"{i} does not exist in our List");
+        }
+      }
+
     }
   }
 }
